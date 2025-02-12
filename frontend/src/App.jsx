eeb5cc10db0e458b3ec869fc53b59d33876f122c
ahgoldmeer <ahgoldmeer@gmail.com>
2025-02-11 23:50:23 -0500

    Changed from plain css to Tailwind CSS

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,24 @@
 import { Link } from "react-router";
-import './App.css';
 import BioGPTImage from './images/BioGPT.png'; // Correctly import the image
 
 function App() {
 	return (
-		<div className="Container">
-			<div className="LeftHalf">
-				<h1 className='Home'>Bioinformatics RAG Chatbot</h1>
-				<div className="Chat">
+		// flex display, background color = white
+		<div className="flex h-screen relative bg-[#fdfbef]">
+			{/* flex-1 = 1/2 length screen, justify-center = center horizontally */} 
+			<div className="flex-1 flex justify-center items-center relative">
+				{/* absolute = fixed position, top-4 = 4px from top, text-3xl = text size, font-bold = bold, underline = underline */} 
+				<h1 className="absolute top-4 text-3xl font-bold underline">
+					Bioinformatics RAG Chatbot
+				</h1>
+				{/* flex display, flex-col = column direction, justify-center = center horizontally, items-center = center vertically, bg-[#44c755] = green color, rounded-2xl = rounded corners, border-2 = 2px border, border-black = black border, p-4 = padding 4px */}
+				<div className="flex justify-center items-center bg-[#44c755] rounded-2xl border-2 border-black p-4">
 					<Link to='/chat'>Begin New Chat</Link>
 				</div>
 			</div>
-			<div className="RightHalf">
-				<img src={BioGPTImage} alt="Description" className="CenteredImage" />
+			{/* flex-1 = 1/2 length screen, justify-center = center horizontally, items-center = center vertically, relative = relative position */}
+			<div className="flex-1 flex justify-center items-center relative bg-[#44c755] rounded-2xl m-4">
+				<img src={BioGPTImage} alt="Description" className="max-w-[90%] max-h-[90%]" />
 			</div>
 		</div>
 	);
