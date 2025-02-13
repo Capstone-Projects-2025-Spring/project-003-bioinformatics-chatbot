5b86d5b90cb822b58ad4740dc9d2a972bba997db
Justin <JustinTruong456@gmail.com>
2025-02-12 21:06:37 -0500

    Added a admin login page

--- backend/app/main/routes.py
+++ backend/app/main/routes.py
@@ -1,24 +1,67 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
 from app.main import bp
 from app.models import User
 from app import db
 
+from flask_wtf import FlaskForm
+from wtforms.validators import InputRequired, Length
+from wtforms import StringField, PasswordField, SubmitField
 
-@bp.route("/", methods=["GET"])
-@bp.route("/index", methods=["GET"])
+
+
+#Logging form that is used in the index.html page
+class LoginForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+
+    password = PasswordField(validators=[InputRequired(), Length(min=5, max=20)], render_kw={"placeholder": "Password"})
+
+    submit = SubmitField('Login')
+
+
+
+
+@bp.route("/", methods=["GET", "POST"])
+@bp.route("/index", methods=["GET", "POST"])
+
+
 def index():
     """
-    Author: Troy Witmer
+    Author: Troy Witmer 
     Date: 02/06/2025
     Description: Sample route, currently index endpoint.
+
+    Author: Justin Truong 
+    Date: 02/12/2025
+    Description: Added a admin login.
+
     """
+    #login form
+    form = LoginForm()
 
-    # how to make a simple query
-    user = User.query.filter_by(username="admin").first()
-    if not user:
-        user = User(username="admin")
-        user.set_password("password")
-        db.session.add(user)
-        db.session.commit()
+    #Check for correct password/username
+    if form.validate_on_submit():
+        if form.username.data == "admin" and form.password.data == "admin":
 
-    return render_template("main/index.html", user=user)
+            # how to make a simple query 
+            user = User.query.filter_by(username="admin").first()
+            if not user:
+                user = User(username="admin")
+                user.set_password("password")
+                db.session.add(user)
+                db.session.commit()
+
+            # Render admin page if login is successful
+            return render_template("main/admin.html", user=user)  
+        else:
+            #return error to index page
+            return render_template("main/index.html", form=form, error="Invalid username or password") 
+
+
+    #Pass the forms here.
+    return render_template("main/index.html", form=form)
+
+
+@bp.route('/logout')
+# Redirect to login page
+def logout():
+    return redirect(url_for('main.index'))  
