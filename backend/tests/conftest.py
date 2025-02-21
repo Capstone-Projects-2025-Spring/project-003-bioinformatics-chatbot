3c841c05e8a7d72048ad3d54e55d758d1b1e3952
T-Witmer <troykwitmer@gmail.com>
2025-02-20 22:52:09 -0500

    updating gitignore for pycache and virtual enviroments

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -5,6 +5,12 @@ from app import create_app, db
 
 @pytest.fixture()
 def app():
+    """
+    Author: Troy Witmer
+    Date: 02/20/2025
+    Description: pytest fixture to create a fresh application and database (sqlite in memory)
+    for each test. then removes drops all db after test.
+    """
     app = create_app(config_class=TestingConfig)
 
     with app.app_context():
@@ -14,3 +20,13 @@ def app():
 
     with app.app_context():
         db.drop_all()
+
+
+@pytest.fixture()
+def client(app):
+    """
+    Author: Troy Witmer
+    Date: 02/20/2025
+    Description: pytest fixture to create a sample client ()
+    """
+    return app.test_client()
