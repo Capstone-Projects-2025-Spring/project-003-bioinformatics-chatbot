7d2e2dee31a4872ab2448c0bbc4902964f45759c
kBunn411 <117461180+kBunn411@users.noreply.github.com>
2025-02-21 10:20:40 -0500

    Updated my test and trying to remove the .pyc

--- frontend/src/tests/ConnectionTest.test.jsx
+++ frontend/src/tests/ConnectionTest.test.jsx
@@ -21,27 +21,11 @@ describe("Flask to React", () => {
     const data = await response.json();
 
 
-    // Making sure that the two messages equal one another
-    expect(data.message).toBe(`Hello: ${username}`);
+    // Making sure that the two messages equal one another, the 2nd message could occur if there is no admin user created yet.
+    expect(data.message).toBeOneOf([`Hello: ${username}`, `No one is here :().`]);
   });
 
-  it("Should get data from backend even if the user has not logged in before", async () => {
-    
-    // Performing request for the data
-    const response = await fetch("http://127.0.0.1:444/test");
-
-
-    // Making sure that the response returns correctly
-    expect(response.status).toBe(200);
-
-
-    // Jsonifying the response to see if there is a match
-    const data = await response.json();
-
-
-    // Making sure that the two messages equal one another
-    expect(data.message).toBe(`No one is here :().`);
-  });
+ 
 
 
 });
