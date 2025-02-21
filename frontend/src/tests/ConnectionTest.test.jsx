64ee49b851193374edf6a6a489d6213fb730b05f
kBunn411 <117461180+kBunn411@users.noreply.github.com>
2025-02-21 00:01:17 -0500

    added .pyc to gitignore and test

--- frontend/src/tests/ConnectionTest.test.jsx
+++ frontend/src/tests/ConnectionTest.test.jsx
@@ -5,7 +5,7 @@ import "@testing-library/jest-dom/vitest";
 // Testing Flask to React
 describe("Flask to React", () => {
  
-  it("Should get data from backend", async () => {
+  it("Should get data from backend if the user has logged in at least once", async () => {
     const username = "admin"; // Right now we are only using the admin login
 
 
@@ -23,6 +23,24 @@ describe("Flask to React", () => {
 
     // Making sure that the two messages equal one another
     expect(data.message).toBe(`Hello: ${username}`);
+  });
+
+  it("Should get data from backend even if the user has not logged in before", async () => {
+    
+    // Performing request for the data
+    const response = await fetch("http://127.0.0.1:444/test");
+
+
+    // Making sure that the response returns correctly
+    expect(response.status).toBe(200);
+
+
+    // Jsonifying the response to see if there is a match
+    const data = await response.json();
+
+
+    // Making sure that the two messages equal one another
+    expect(data.message).toBe(`No one is here :().`);
   });
 
 
