1a073feddf46aa5a90e953414e75cd2f4742c369
T-Witmer <troykwitmer@gmail.com>
2025-03-02 20:07:11 -0500

    fixing tests and file paths for refactoring parsing

--- backend/tests/parser_test.py
+++ backend/tests/parser_test.py
@@ -1,12 +1,14 @@
 import sys
 import os
 import pytest
+
 # Add the project root to Python's module path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-#Getting the parse function from the py files
-from parse_index_files.parse_pdf import parse_pdf  
-from parse_index_files.parse_txt import parse_txt  
+# Getting the parse function from the py files
+from app.doc_parsers.parse_pdf import parse_pdf
+from app.doc_parsers.parse_txt import parse_txt
+
 
 def test_parse_pdf():
     """
@@ -14,32 +16,33 @@ def test_parse_pdf():
     test the parse_pdf
 
     """
-    FILE_PATH = './parse_index_files/data/Dna.pdf'
+    FILE_PATH = "./app/doc_parsers/data/Dna.pdf"
 
-     # Call the function you want to test
+    # Call the function you want to test
     result = parse_pdf(FILE_PATH)
-    
+
     # Example test assertion - check if result is not empty
     assert len(result) > 0, "No PDFs were parsed."
 
     # Example test assertion - check if result is a list
     assert isinstance(result, list), "Result is not a list."
 
-def test_parse_txt():
 
+def test_parse_txt():
     """
 
     test the parse_txt
 
     """
 
-    FILE_PATH = './parse_index_files/data/Dna.txt'
+    FILE_PATH = "./app/doc_parsers/data/Dna.txt"
 
-     # Call the function you want to test
+    # Call the function you want to test
     result = parse_txt(FILE_PATH)
-    
+
     # Example test assertion - check if result is not empty
     assert len(result) > 0, "No txts were parsed."
 
     # Example test assertion - check if result is a list
     assert isinstance(result, list), "Result is not a list."
+
