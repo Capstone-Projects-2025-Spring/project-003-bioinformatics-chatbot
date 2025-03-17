41743a7c12c29d78d09a188e0922b404264d0789
T-Witmer <troykwitmer@gmail.com>
2025-03-17 13:20:58 -0400

    padding change

--- frontend/src/Components/loadingSpinner.jsx
+++ frontend/src/Components/loadingSpinner.jsx
@@ -1,6 +1,6 @@
 export default function LoadingSpinner() {
   return (
-    <div className="flex space-x-2 justify-left items-end bg-gray-800 pl-3" data-testid="spinner">
+    <div className="flex space-x-2 justify-left items-end bg-gray-800 pl-4" data-testid="spinner">
       <span className="sr-only">Loading...</span>
       <div className="h-3 w-3 bg-gray-200 rounded-full animate-bounce" style={{ animationDelay: "-0.3s" }}></div>
       <div className="h-3 w-3 bg-gray-200 rounded-full animate-bounce" style={{ animationDelay: "-0.15s" }}></div>
