d5b4cb0f41de76b65baa57008935b551a2d3df87
Justin <JustinTruong456@gmail.com>
2025-03-17 20:45:10 -0400

    Fix pdf files format by creating pdf files seperately

--- backend/tests/test_database_data.py
+++ backend/tests/test_database_data.py
@@ -1,15 +1,28 @@
 import io
 from app.models import Document  
+from werkzeug.datastructures import FileStorage
+import os
 
 
 def test_content_file(client, app):
     app.config['WTF_CSRF_ENABLED'] = False # In testing envionment, no need for CSRF_TOKEN
     with app.app_context(): 
         
-        # Simulation of file upload
+        # Path to the test file inside the test_data folder
+        file_path = os.path.join(os.path.dirname(__file__), 'test_data', 'test.pdf')
+
+        # Open the file in binary read mode
+        with open(file_path, 'rb') as f:
+            # Create a FileStorage object from the file content
+            pdf_file = FileStorage(
+                stream=io.BytesIO(f.read()),
+                filename="test.pdf",
+                content_type="application/pdf",
+            )
+        
+        # Prepare the data dictionary with the file for upload
         data = {
-            "pdf_file": (io.BytesIO(b"I like candy"), "test.pdf"),
-            
+            "pdf_file": (io.BytesIO(pdf_file.read()), "test.pdf"),
         }
 
         # Post request testing the /uplouad route
