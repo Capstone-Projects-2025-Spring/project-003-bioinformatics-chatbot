5ba6becf06cbe305ee50544466775e4445c34db0
ishmam02 <kabirishmam@gmail.com>
2025-04-01 19:20:56 -0400

    Changed doc retrieval function to look for relevance

--- backend/app/doc_indexer/retrieve_document.py
+++ backend/app/doc_indexer/retrieve_document.py
@@ -3,4 +3,4 @@ from flask import current_app
 
 def query_database(query_text, k=3):
     vector_db = current_app.vector_db
-    return vector_db.similarity_search_with_score(query_text, k=k)
+    return vector_db.similarity_search_with_relevance_scores(query_text, k=k)
