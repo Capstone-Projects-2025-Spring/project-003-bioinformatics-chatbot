0349b33b6ef9d8b2cecaa106c6dd5c9dc890d3ad
Khanh Nguyen <nguyenquockenken@gmail.com>
2025-04-21 17:40:41 -0400

    before demo 3

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,5 +1,13 @@
 import axios from "axios";
 
+// const api = axios.create({
+// 	baseURL: "https://bscb.site/chat",
+// });
+// adminURL = "https://chat.bscb.site";
+
+//export default api;
+
+
 export default axios.create({
 	baseURL: "https://chat.bscb.site/chat",
 	adminURL: "https://chat.bscb.site",
