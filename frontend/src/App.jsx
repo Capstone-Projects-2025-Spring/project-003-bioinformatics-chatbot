149bbc3c262d94d5ef219b40a8667582a40a014d
kBunn411 <117461180+kBunn411@users.noreply.github.com>
2025-04-27 14:05:03 -0400

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -857,7 +857,7 @@ function App() {
         <div
           className='flex justify-center h-full overflow-y-auto '
           data-testid='messageContainer'>
-          <div className='w-full py-2 px-4 max-w-7xl mx-auto mt-7'>
+          <div className='w-full py-2 px-25 max-w-7xl mx-auto mt-7'>
             {messages.map((msg, index) =>
               msg.type === "Question" ? (
                 <UserBubble
